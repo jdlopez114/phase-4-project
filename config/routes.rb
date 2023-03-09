cec8ccc972ebd692f09527c9f86e1f92c5ade48d
Josiel <jdlopez114@gmail.com>
2023-03-09 00:29:17 -0500

    added authorize

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
  
-  # resources :reviews
+  resources :reviews
   resources :cars
-   # get "/cars", to: "cars#index"
-  # resources :users
-  
+  resources :users
+
+  # post "/login", to: "sessions#create"
+  # delete "/logout", to: "sessions#destroy"
+
+  # post "/signup", to: "users#create"
+  # get "/me", to: "users#show"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
